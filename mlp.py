ca2522cb75f228a803c1b7cb87488b3365074393
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2019-04-26 15:46:42 +0200

    add support for more layers, closes #2

--- mlp.py
+++ mlp.py
@@ -10,18 +10,28 @@ class Mlp:
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
 
-        # Initialize random weights for each layer
-        self.weights_hidden = np.random.randn(hidden_nodes, input_nodes) * np.sqrt(2 / (hidden_nodes + input_nodes))
-        # self.weights_hidden = np.random.uniform(-1, 1, size=(hidden_nodes, input_nodes))
-        self.weights_output = np.random.randn(output_nodes, hidden_nodes) * np.sqrt(2 / (output_nodes + hidden_nodes))
-        # self.weights_output = np.random.uniform(-1, 1, size=(output_nodes, hidden_nodes))
+        self.number_of_nodes = [input_nodes, hidden_nodes, output_nodes]
 
-        # Initialize random bias for each layer
-        self.bias_hidden = np.random.uniform(0, 0, size=(hidden_nodes, 1))
-        self.bias_output = np.random.uniform(0, 0, size=(output_nodes, 1))
+        self.weights = []
+        self.weights.append(np.random.randn(hidden_nodes, input_nodes) * np.sqrt(2 / (hidden_nodes + input_nodes)))
+        self.weights.append(np.random.randn(output_nodes, hidden_nodes) * np.sqrt(2 / (output_nodes + hidden_nodes)))
+
+        self.biases = []
+        self.biases.append(np.random.uniform(0, 0, size=(hidden_nodes, 1)))
+        self.biases.append(np.random.uniform(0, 0, size=(output_nodes, 1)))
+
+        self.output = []
+        self.errors = []
 
         # How much every train iteration contributes to changing weights
         self.learning_rate = learning_rate
+
+    def add_layer(self, number_of_nodes):
+        self.number_of_nodes.insert(-1, number_of_nodes)
+        self.weights.append([])
+        for i in range(2):
+            self.weights[-1-i] = np.random.randn(self.number_of_nodes[-1-i], self.number_of_nodes[-2-i]) * np.sqrt(2 / (self.number_of_nodes[-1-i] + self.number_of_nodes[-2-i]))
+        self.biases.insert(-1, np.random.uniform(0, 0, size=(number_of_nodes, 1)))
 
     @staticmethod
     def sigmoid_f(x):
@@ -36,60 +46,31 @@ class Mlp:
         return sig(x)
 
     def feed_forward(self, inp):
-        inp = np.matrix(inp).transpose()
-        # Calculate hidden layer linear outputs
-        hidden_outputs = np.dot(self.weights_hidden, inp)
-        # Add hidden layer bias
-        hidden_outputs = hidden_outputs + self.bias_hidden
-        # Apply squashing function
-        hidden_outputs = self.sigmoid(hidden_outputs)
+        outputs = [np.matrix(inp).transpose()]
 
-        # Calculate output layer linear outputs
-        output = np.dot(self.weights_output, hidden_outputs)
-        # Add output layer bias
-        output = output + self.bias_output
-        # Apply squashing function
-        output = self.sigmoid(output)
+        for i in range(len(self.number_of_nodes) - 1):
+            outputs.append(self.sigmoid((np.dot(self.weights[i], outputs[-1]) + self.biases[i])))
 
-        # Return the layers' outputs, the hidden one is needed during back-propagation
-        return hidden_outputs, output
+        return outputs
 
     # This training function uses stochastic gradient descent instead of batch
     def train(self, inp, targets):
         targets = np.matrix(targets).transpose()
         # Calculate output with given input
-        hidden_outputs, output = self.feed_forward(inp)
-        inp = np.matrix(inp).transpose()
-        # Calculate errors between predicted and real values
-        output_errors = np.subtract(targets, output)
+        outputs = self.feed_forward(inp)
 
-        # Calculate gradient, first sigmoid derivative
-        gradient = np.multiply(output, (1 - output))
-        gradient = np.multiply(output_errors, gradient)
-        # Multiply by the learning rate
-        gradient = gradient * self.learning_rate
-        # Calculate hidden to output weights changes 
-        delta_weights_output = np.dot(gradient, hidden_outputs.transpose())
-        # The new weights are just the sum between the old ones and the deltas
-        self.weights_output = self.weights_output + delta_weights_output
-        # Changes in bias are just the gradient
-        self.bias_output = self.bias_output + gradient
+        errors = [np.subtract(targets, outputs[-1])]
+        for i in range(len(self.weights) - 1):
+            errors.insert(0, np.dot(self.weights[-1-i].transpose(), errors[0]))
 
-        # Calculate the errors in input to hidden layer
-        weights_output_t = self.weights_output.transpose()
-        hidden_errors = np.dot(weights_output_t, output_errors)
-
-        # Same calculus as before, first the gradient
-        hidden_gradient = np.multiply(hidden_outputs, (1 - hidden_outputs))
-        hidden_gradient = np.multiply(hidden_errors, hidden_gradient)
-        hidden_gradient = np.multiply(hidden_gradient, self.learning_rate)
-        # Weights changes in input to hidden
-        delta_weights_hidden = np.dot(hidden_gradient, inp.transpose())
-        # Sum the error
-        self.weights_hidden = self.weights_hidden + delta_weights_hidden
-        # The bias change according to the gradient
-        self.bias_hidden = self.bias_hidden + hidden_gradient
+        for i in range(len(self.weights)):
+            tmp = np.multiply(outputs[-1-i], (1 - outputs[-1-i]))
+            gradient = np.multiply(errors[-1-i], tmp)
+            gradient *= self.learning_rate
+            self.biases[-1-i] += gradient
+            delta_w  = np.dot(gradient, outputs[-2-i].transpose())
+            self.weights[-1-i] += delta_w
 
     def predict(self, inp):
-        hidden_outputs, output = self.feed_forward(inp)
-        return output
+        outputs = self.feed_forward(inp)
+        return outputs[-1]
