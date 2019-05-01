2bd6353b16bfd88d1d28ed4e1d35d4cf72c64a33
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2019-05-01 10:26:09 +0200

    add different squash function, close #3

--- mlp.py
+++ mlp.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import operator
+import re
 
 class Mlp:
     def __init__(self, init_nodes: int = 0, learning_rate: float = .2) -> None:
@@ -9,15 +10,18 @@ class Mlp:
             self.number_of_nodes.append(init_nodes)
         self.weights = []
         self.biases = []
+        self.functions = []
         self.learning_rate = learning_rate
 
-    def add_layer(self, number_of_nodes: int, weights = None, bias = None):
+    def add_layer(self, number_of_nodes: int, weights = None, bias = None, function="sigmoid"):
         self.number_of_nodes.append(number_of_nodes)
         if not weights is None:
             self.weights.append(weights)
+            self.functions.append(function)
         elif len(self.number_of_nodes) > 1:
             self.weights.append(np.random.randn(self.number_of_nodes[-1], self.number_of_nodes[-2]) * np.sqrt(2 / (self.number_of_nodes[-1] + self.number_of_nodes[-2])))
-        
+            self.functions.append(function)
+
         if not bias is None:
             self.biases.append(bias)
         elif len(self.number_of_nodes) > 1:
@@ -27,6 +31,9 @@ class Mlp:
         f = open(location, "w+")
         for i in self.number_of_nodes:
             f.write(str(i) + " ")
+        f.write("\t")
+        for i in self.functions:
+            f.write(i + " ")
         f.write("\n")
         for i in self.weights:
             for j in i:
@@ -45,8 +52,10 @@ class Mlp:
     def load(location):
         f = open(location, "r")
         lines = f.readlines()
-    
-        number_of_nodes = np.vectorize(lambda x: int(x))( lines[0].strip().split() )
+        f_l = lines[0].strip()
+        f_l = re.split(r'\t+', f_l)
+        number_of_nodes = np.vectorize(lambda x: int(x))( f_l[0].split() )
+        functions = f_l[1].split()
         weigths = []
         for i in range(1, len(number_of_nodes)):
             m = lines[i].strip().split("\t")
@@ -59,33 +68,55 @@ class Mlp:
             b = lines[i].strip().split("\t")
             for j in range(len(b)):
                 b[j] = b[j].split()
-            b = np.vectorize(lambda x: float(x))(np.matrix(b))
+            b = np.vectorize(lambda x: float(x))(np.matrix(b).T)
             biases.append(b)
         nn = Mlp()
         for i in range(len(number_of_nodes)):
             if i > 0:
-                nn.add_layer(number_of_nodes[i], weigths[i-1], biases[i-1])
+                nn.add_layer(number_of_nodes=number_of_nodes[i], weights=weigths[i-1], bias=biases[i-1], function=functions[i-1])
             else:
                 nn.add_layer(number_of_nodes[i])
         return nn
 
     @staticmethod
-    def sigmoid_f(x):
-        # Standard sigmoid function
-        if x < 0:
-            return 1 - 1 / (1 + math.exp(x))
-        else:
-            return 1 / (1 + math.exp(-x))
+    def soft_plus(x):
+        sp = np.vectorize(lambda y: math.log(1 + math.exp(y)))
+        return sp(x)
 
-    def sigmoid(self, x):
-        sig = np.vectorize(lambda y: self.sigmoid_f(y))
+    @staticmethod
+    def relu(x):
+        re = np.vectorize(lambda y: max(0, y))
+        return re(x)
+
+    @staticmethod
+    def sigmoid(x):
+        sig = np.vectorize(lambda y:  (1 - 1 / (1 + math.exp(y))) if y < 0 else  (1 / (1 + math.exp(-y))))
         return sig(x)
+    
+    @staticmethod
+    def squash(x, function):
+        if function == "sigmoid":
+            return Mlp.sigmoid(x)
+        elif function == "soft_plus":
+            return Mlp.soft_plus(x)
+        elif function == "relu":
+            return Mlp.relu(x)
+
+    @staticmethod
+    def derivative(x, function):
+        if function == "sigmoid":
+            return np.multiply(x, (1-x))
+        elif function == "soft_plus":
+            return Mlp.sigmoid(x)
+        elif function == "relu":
+            d_relu = np.vectorize(lambda y: 1 if y > 0 else 0)
+            return d_relu(x)
 
     def feed_forward(self, inp):
         outputs = [np.matrix(inp).T]
 
         for i in range(len(self.number_of_nodes) - 1):
-            outputs.append(self.sigmoid((np.dot(self.weights[i], outputs[-1]) + self.biases[i])))
+            outputs.append(Mlp.squash((np.dot(self.weights[i], outputs[-1]) + self.biases[i]), self.functions[i]))
 
         return outputs
 
@@ -102,7 +133,7 @@ class Mlp:
 
         for i in range(len(self.weights)):
             # Calculate gradient and weight correction
-            gradient = np.multiply(errors[-1-i], np.multiply(outputs[-1-i], (1 - outputs[-1-i])))
+            gradient = np.multiply(errors[-1-i], Mlp.derivative(outputs[-1-i], self.functions[-1-i]))
             gradient *= self.learning_rate
             self.biases[-1-i] += gradient
             delta_w  = np.dot(gradient, outputs[-2-i].T)
